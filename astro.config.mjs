5364e768a555320a30c1f5a76a27c656ac57b881
Dion Ridley <dridley@gmail.com>
2026-02-16 12:01:19 -0800

    Added intial pacakges

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import compress from 'astro-compress';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://emergentworks.org', // required for sitemap
+  integrations: [react(), sitemap(), mdx(), compress()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
